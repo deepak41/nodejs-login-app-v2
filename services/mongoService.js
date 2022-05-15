88264d88f30dc9ef43d437d1662a382fb109164a
DEEPAK KUMAR SINGH <deepak.singh@getvymo.com>
2022-05-15 22:35:45 +0530

    updated mongoose

--- services/mongoService.js
+++ services/mongoService.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 const MONGO_URI = 'mongodb://localhost/nodejs-login-app-v2';
 
 const mongoConnect = () => {
-    mongoose.connect(MONGO_URI, {useMongoClient: true}, function(err) {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, (err) => {
         if(err) {
             return console.log(`Mongoose - Connection Error: ${MONGO_URI}`);
         }
         console.log(`Mongoose - Connection established at ${MONGO_URI}`);
     });
-};
+}
 
 module.exports = {
     mongoConnect: mongoConnect
